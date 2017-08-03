5ff0ebaeebfe98800d2bbdb301c1974ffaf6dc54
GAVIN LUNA <GAVIN LUNA>
2017-08-03 19:04:50 -0400

    directories

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log('this is loaded');
+var cunsumer_key = 
+exports.twitterKeys = {
+  consumer_key: 
+  consumer_secret: 
+  access_token_key: 
+  access_token_secret: 
---
+++ liri.js
@@ -0,0 +1,3 @@
+var liri = process.argv[2];
+var command = process.argv[3];
+
